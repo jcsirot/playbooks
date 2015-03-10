e4c89a62697cf409b3197905f9da418ea0641f5f
florent claerhout <contact@fclaerhout.fr>
2015-03-10 10:28:57 +0100

    add flush

--- analyzer/nat.sh
+++ analyzer/nat.sh
@@ -18,6 +18,7 @@ IPFWD=/proc/sys/net/ipv4/ip_forward
 case ${1:-usage} in
 	start)
 		echo 1 > $IPFWD
+		iptables --flush
 		iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 		iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 		iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
